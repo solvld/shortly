16b0cc34cd59577675b389fca002f950c10a79df
solvld <sol.vld@yandex.ru>
2024-01-13 14:37:03 +0400

    edit InputField component

--- src/Components/InputField/InputField.jsx
+++ src/Components/InputField/InputField.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import Button from '../Button/Button'
+import React from 'react';
+import Button from '../Button/Button';
+import Wrapper from '../Wrapper/Wrapper';
 
 function InputField() {
   return (
-		<div className=' bg-slate-200 p-4 h-screen'>
-			<div className=' flex justify-center bg-very-dark-blue p-8'>
-				<input
-					className='p-2 rounded-lg mr-4'
-					type='text'
-					placeholder='Shorten a link here...'
-				/>
-				<Button roundBorder={false}>Shorten it!</Button>
-			</div>
-		</div>
-	)
+    <div className="rounded-lg flex justify-center bg-very-dark-blue p-8 mb-6">
+      <input
+        className="p-2 rounded-lg mr-4"
+        type="text"
+        placeholder="Shorten a link here..."
+      />
+      <Button roundBorder={false}>Shorten it!</Button>
+    </div>
+  );
 }
 
-export default InputField
+export default InputField;
