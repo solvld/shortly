eadd66d87a5626a38825bcd2139c9b97764ec577
solvld <sol.vld@yandex.ru>
2024-01-13 14:40:49 +0400

    add LinksSection in app template

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import './App.scss'
-import Wrapper from './Components/Wrapper/Wrapper'
-import Header from './Components/Header/Header'
-import MainSection from './Components/MainSection/MainSection'
+import Wrapper from './Components/Wrapper/Wrapper';
+import Header from './Components/Header/Header';
+import MainSection from './Components/MainSection/MainSection';
+import InputField from './Components/InputField/InputField';
+import LinksSection from './Components/LinksSection/LinksSection';
 
 function App() {
-
   return (
-		<Wrapper>
-      <Header />
-      <MainSection />
-    </Wrapper>
-	)
+    <>
+      <Wrapper>
+        <Header />
+        <MainSection />
+      </Wrapper>
+      <LinksSection />
+    </>
+  );
 }
 
-export default App
+export default App;
