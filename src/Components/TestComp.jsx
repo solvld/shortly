33162c9931dabd6cfdb1911ec94043d2d3b9882b
solvld <sol.vld@yandex.ru>
2024-01-14 22:56:59 +0400

    edit for test

--- src/Components/TestComp.jsx
+++ src/Components/TestComp.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const stats = [
-  { id: 1, name: 'Transactions every 24 hours', value: '44 million' },
-  { id: 2, name: 'Assets under holding', value: '$119 trillion' },
-  { id: 3, name: 'New users annually', value: '46,000' },
+  { id: 1, name: 'Transactions every 24 hours', value: '33 million' },
+  { id: 2, name: 'Test branch', value: 'Test branch' },
+  { id: 3, name: 'New users annually', value: '1,000' },
 ]
 
 function TestComp() {
